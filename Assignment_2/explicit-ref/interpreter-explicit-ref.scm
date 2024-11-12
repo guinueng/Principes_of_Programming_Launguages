4767d9d545ee9c00a39db47538ac39968f319fa4
Guinueng <admin@guinueng.com>
2024-11-12 11:32:50 +0900

    Skeleton code for Assignment #2.

---
+++ Assignment_2/explicit-ref/interpreter-explicit-ref.scm
@@ -0,0 +1,51 @@
+#lang racket
+
+(require eopl)
+(require "datatype.scm")
+(require "environments.scm")
+(require "utils.scm")
+(require "store.scm")
+
+(provide value-of-program value-of)
+
+;; value-of-program : Program * Env -> ExpVal
+(define value-of-program
+  (lambda (pgm env)
+    (initialize-store!)
+    (cases program pgm
+      (a-program (exp)
+                 (value-of exp env)))))
+
+;; value-of : Exp * Env -> ExpVal
+(define value-of
+  (lambda (exp env)
+    (debug "Evaluating exp: ~s, env: ~s~n" exp env)
+    (cases expression exp
+      (const-exp (num) (num-val num))
+      (add-exp (exp1 exp2)
+               (let ((result (+ (expval->num (value-of exp1 env))
+                                (expval->num (value-of exp2 env)))))
+                 (num-val result)))
+      (diff-exp (exp1 exp2)
+                (let ((result (- (expval->num (value-of exp1 env))
+                                 (expval->num (value-of exp2 env)))))
+                  (num-val result)))
+      (zero?-exp (exp)
+                 (let ((result (zero? (expval->num (value-of exp env)))))
+                   (bool-val result)))
+      (less-than-exp (exp1 exp2)
+                     (let ((result (< (expval->num (value-of exp1 env))
+                                      (expval->num (value-of exp2 env)))))
+                       (bool-val result)))
+      (not-exp (exp)
+               (let ((result (not (expval->bool (value-of exp env)))))
+                 (bool-val result)))
+      (if-exp (test then else)
+              (if (expval->bool (value-of test env))
+                  (value-of then env)
+                  (value-of else env)))
+      (lambda-exp (bound-var body)
+                  (proc-val (procedure bound-var body env)))
+      (else (error 'value-of "Unsupported expression: ~s" exp)))))
+
+
